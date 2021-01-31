eff9922fcde4a8200bf6a72eb024eea323b12a91
RaymondLoranger <Raymond.Loranger@bell.net>
2021-01-31 17:24:51 -0500

    0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhxConfig.MixProject do
   def project do
     [
       app: :phx_config,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Phx Config",
